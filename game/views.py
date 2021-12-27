4c600155c2b2cb14fb6049637e94984bb2257fc6
ema.fazz <ema.fazz@gmail.com>
2021-12-27 13:26:54 +0100

    Creation of the baseline HTML pages

--- game/views.py
+++ game/views.py
@@ -1,3 +1,13 @@
+from django.http import request
+from django.http.response import HttpResponse
 from django.shortcuts import render
 
-# Create your views here.
+
+
+def index(request):
+
+    return render(request, 'index.html')
+
+def lobby(request):
+
+    return render(request, 'lobby-room.html')
