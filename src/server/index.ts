0f6199b6b1843f5d94bfa67467a73eeabdc59b7d
Michele <bonini.michele15@icloud.com>
2022-01-04 20:20:43 +0100

    Start project V1 with Server Configuration

---
+++ src/server/index.ts
@@ -0,0 +1,32 @@
+// Colyseus + Express
+import http from "http";
+import { Server } from "colyseus";
+import express from "express";
+import cors from 'cors'
+import { monitor } from "@colyseus/monitor";
+import { MyRoom } from './rooms/MyRoom'
+
+
+const port = Number(process.env.port || 2567);
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+//app.use('/static', express.static('public'))
+const server = http.createServer(app)
+const gameServer = new Server({    
+    server,
+});
+
+app.get('/', (req, res) => {
+    res.send('Hello x!')
+})
+
+gameServer.define('my_room', MyRoom);
+
+app.use('/colyseus', monitor())
+
+gameServer.listen(port)
+
+console.log(`Listening on ws://localhost:${port}`)
---
+++ src/server/rooms/MyRoom.ts
@@ -0,0 +1,31 @@
+import { Room, Client, RedisPresence } from "colyseus";
+import { MyRoomState } from "./schema/MyRoomState";
+
+export class MyRoom extends Room<MyRoomState> {
+
+  onCreate (options: any) {
+    this.setState(new MyRoomState());
+
+    this.onMessage("type", (client, message) => {
+      //
+      // handle "type" message
+      //
+    });
+
+  }
+
+  onJoin (client: Client, options: any) {
+    console.log(client.sessionId, " joined!");
+    console.log(options.name, " joined!");
+
+  }
+
+  onLeave (client: Client, consented: boolean) {
+    console.log(client.sessionId, "left!");
+  }
+
+  onDispose() {
+    console.log("room", this.roomId, "disposing...");
+  }
+
+}
