77cb2764e35b671e4904d54141b635daee2a9684
ema.fazz <ema.fazz@gmail.com>
2022-01-05 18:49:31 +0100

    Adaptation for express.js environment
    Please enter the commit message for your changes. Lines starting

--- src/server/index.ts
+++ src/server/index.ts
@@ -5,6 +5,7 @@ import express from "express";
 import cors from 'cors'
 import { monitor } from "@colyseus/monitor";
 import { GameRoom } from './rooms/GameRoom'
+import path from 'path'
 
 
 const port = Number(process.env.port || 2567);
@@ -12,6 +13,7 @@ const port = Number(process.env.port || 2567);
 const app = express();
 app.use(cors());
 app.use(express.json());
+app.use("/static",express.static(__dirname+'../client/static'))
 
 //app.use('/static', express.static('public'))
 const server = http.createServer(app)
@@ -20,7 +22,8 @@ const gameServer = new Server({
 });
 
 app.get('/', (req, res) => {
-    res.send('Hai sbagliato... go to: http://127.0.0.1:2567/colyseus/#/')
+    //res.send('Hai sbagliato... go to: http://127.0.0.1:2567/colyseus/#/')
+    res.sendFile(path.join(__dirname, '../client/game.html'));
 })
 
 gameServer.define('game_room', GameRoom);
