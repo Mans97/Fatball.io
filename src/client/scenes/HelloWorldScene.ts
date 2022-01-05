2f46d1a6f3cb1ea92853b070d543bc023fd7cc6f
Michele <bonini.michele15@icloud.com>
2022-01-05 15:24:37 +0100

    parcel deleted

--- src/client/scenes/HelloWorldScene.ts
+++ src/client/scenes/HelloWorldScene.ts
@@ -1,8 +1,15 @@
 import Phaser from 'phaser'
 import * as Colyseus from 'colyseus.js'
 
+
+
+import { State } from '../../server/rooms/schema/State'
+
 export default class HelloWorldScene extends Phaser.Scene
 {
+    declare private cursors:any 
+    declare private player:any 
+    declare private circle_object:any
     private client: Colyseus.Client
 	constructor()
 	{
@@ -18,17 +25,43 @@ export default class HelloWorldScene extends Phaser.Scene
     {
         //this.load.setBaseURL('http://labs.phaser.io')
         //this.load.image('sky', 'assets/skies/space3.png')
-
-
-        
     }
+
 
     async create()
     {
         //Join room
-        const room = await this.client.joinOrCreate('my_room') //if there is one in the room, I have to use joinOrCreate()
+        const room = await this.client.joinOrCreate('game_room') //if there is one in the room, I have to use joinOrCreate()
 
         console.log(room.sessionId) //id of connectedplayes, esiste anche room.name
+
+        
+
+
+        room.state.players.onAdd = (player, sessionId: string) => {
+            console.log("player ",player)
+            console.log("\tenter in onAdd")
+            
+            //create the player
+            this.circle_object = this.add.circle(0, 0, 50, 0xaaaaaa).setStrokeStyle(3, 0xeaa66aa);
+            this.player = this.physics.add.existing(this.circle_object);
+
+            //players[sessionId] = player
+
+            //this.player.setCollideWorldBounds(true);
+            this.cameras.main.startFollow(this.player)
+        
+        }
+        
+
+        room.state.players.onRemove = (player, sessionId) => {
+            delete players[sessionId];
+        }
+
+        room.onStateChange((state) => {
+            console.log("the room state has been updated:", state);
+        });
+
 
         //message coming from the server
         room.onMessage('keydown', (message) => {
@@ -39,5 +72,40 @@ export default class HelloWorldScene extends Phaser.Scene
             room.send('keydown', evt.key) //lo mando a Colysius server
         })
 
+
+        /**
+         * 
+         * FATBALL
+         * 
+         */
+        
+        this.physics.world.setBounds(0, 0, 1000, 1000); // set outer bounds
+
+        // ------------ keyboard setting ------------
+        this.cursors = this.input.keyboard.addKeys("W,A,S,D");
+        console.log(this.cursors)
+
+        
+
+    }
+
+    async update(){
+        
+        if(this.cursors){
+            if( this.cursors.D.isDown) {
+                //room.send("move", { y: -1 });
+                this.player.x += 5;
+            }
+            if( this.cursors.A.isDown) {
+                this.player.x -= 5;          
+            }
+            if( this.cursors.W.isDown) {
+                this.player.y -= 5;
+            }
+            if( this.cursors.S.isDown) {
+                this.player.y += 5;
+            }
+        }
+        
     }
 }
