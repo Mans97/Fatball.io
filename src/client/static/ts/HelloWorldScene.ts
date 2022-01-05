39564fd3032b15169ecd2c4dfa9d94360881f100
Michele <bonini.michele15@icloud.com>
2022-01-05 23:13:14 +0100

    Game assets settings

--- src/client/static/ts/HelloWorldScene.ts
+++ src/client/static/ts/HelloWorldScene.ts
@@ -6,9 +6,11 @@ import { State } from '../../../server/rooms/GameRoom'
 export default class HelloWorldScene extends Phaser.Scene
 {
     declare private cursors:any 
-    declare private player:any 
+    players: { [id: string]: Phaser.Geom.Circle } = {}
+    currentPlayer: Phaser.Geom.Circle
     declare private circle_object:any
     private client: Colyseus.Client
+    room: any
 	constructor()
 	{
 		super('hello-world')
@@ -25,50 +27,66 @@ export default class HelloWorldScene extends Phaser.Scene
         //this.load.image('sky', 'assets/skies/space3.png')
     }
 
+    
 
     async create()
     {
         //Join room
-        const room = await this.client.joinOrCreate<State>('game_room') //if there is one in the room, I have to use joinOrCreate()
+        this.room = await this.client.joinOrCreate<State>('game_room') //if there is one in the room, I have to use joinOrCreate()
 
-        console.log(room.sessionId) //id of connectedplayes, esiste anche room.name
+        console.log(this.room.sessionId) //id of connectedplayes, esiste anche room.name
 
-        
-
-
-        room.state.players.onAdd = (player, sessionId: string) => {
-            console.log("player ",player)
+        this.room.state.players.onAdd = (player: any, sessionId: string) => {
             console.log("\tenter in onAdd")
             
             //create the player
-            this.circle_object = this.add.circle(0, 0, 50, 0xaaaaaa).setStrokeStyle(3, 0xeaa66aa);
-            this.player = this.physics.add.existing(this.circle_object);
+            //this.circle_object = this.add.circle(0, 0, 50, 0xaaaaaa).setStrokeStyle(3, 0xeaa66aa);
+            //this.players = this.physics.add.existing(this.circle_object);
 
-            //players[sessionId] = player
 
-            //this.player.setCollideWorldBounds(true);
-            this.cameras.main.startFollow(this.player)
+            var graphics = this.add.graphics({ fillStyle: { color: 0xff0000}})
+            this.players[sessionId] = new Phaser.Geom.Circle(player.x, player.y, player.radius)
+            graphics.fillCircleShape(this.players[sessionId])
+            //NON VA this.physics.add.existing(graphics)
+
+            //NON VAthis.players[sessionId] = this.physics.add.sprite(300, 100, 'dude')
+
+            
+            //this.players[sessionId].body.collideWorldBounds = true;
+            //this.players2[sessionId].setCollideWorldBounds(true);
+            //this.cameras.main.startFollow(this.players[sessionId])
+
+
+            this.currentPlayer = this.players[sessionId]
+            graphics.fillCircleShape(this.currentPlayer)
+            this.cameras.main.startFollow(this.currentPlayer)
         
         }
         
+        
 
-        room.state.players.onRemove = (player, sessionId) => {
-            delete players[sessionId];
+        this.room.state.players.onRemove = (player: any, sessionId: any) => {
+            delete this.players[sessionId];
         }
 
-        room.onStateChange((state) => {
+        this.room.onStateChange((state: any) => {
             console.log("the room state has been updated:", state);
         });
 
 
         //message coming from the server
-        room.onMessage('keydown', (message) => {
+        /*this.room.onMessage('keydown', (message: any) => {
             console.log(message)
-        })
+        })*/
 
-        this.input.keyboard.on('keydown', (evt: KeyboardEvent) =>{
-            room.send('keydown', evt.key) //lo mando a Colysius server
-        })
+        /*this.input.keyboard.on('keydown', (evt: KeyboardEvent) =>{
+            this.room.send('keydown', evt.key) //lo mando a Colysius server
+        })*/
+
+        this.room.onMessage("move", (message: any) => {
+            console.log("\t MOVED RECEIVED: message received from server");
+            console.log("pony", message);
+        });
 
 
         /**
@@ -76,7 +94,9 @@ export default class HelloWorldScene extends Phaser.Scene
          * FATBALL
          * 
          */
-        
+        var bound_rect = this.add.rectangle(1000, 1000, 6000, 6000);  // draw rectangle around bounds
+        bound_rect.setStrokeStyle(4000, 0x343a40); // stylize
+
         this.physics.world.setBounds(0, 0, 1000, 1000); // set outer bounds
 
         // ------------ keyboard setting ------------
@@ -91,19 +111,33 @@ export default class HelloWorldScene extends Phaser.Scene
         
         if(this.cursors){
             if( this.cursors.D.isDown) {
-                //room.send("move", { y: -1 });
-                this.player.x += 5;
-            }
-            if( this.cursors.A.isDown) {
-                this.player.x -= 5;          
-            }
-            if( this.cursors.W.isDown) {
-                this.player.y -= 5;
-            }
-            if( this.cursors.S.isDown) {
-                this.player.y += 5;
+                this.room.send("move", { y: +5 });
+                this.currentPlayer.x += 5;
             }
         }
         
+        /*if(this.cursors){
+            if( this.cursors.D.isDown) {
+                //room.send("move", { y: -1 });
+                this.currentPlayer.x += 5;
+            }
+            if( this.cursors.A.isDown) {
+                this.currentPlayer.x -= 5;          
+            }
+            if( this.cursors.W.isDown) {
+                this.currentPlayer.y -= 5;
+            }
+            if( this.cursors.S.isDown) {
+                this.currentPlayer.y += 5;
+            }
+        }*/
+        
     }
+
+ 
+    
+
 }
+
+
+
