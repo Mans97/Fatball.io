45933b84db080999fae03deed1602810111a900c
kode-git <mariosessa64@gmail.com>
2022-01-07 09:27:36 -0800

    Connections management in lobby session on one instance

--- src/server/index.ts
+++ src/server/index.ts
@@ -14,6 +14,7 @@ const host = 'localhost'
 const app = express();
 app.use(cors());
 app.use(express.json());
+app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.static(__dirname+'/../client/static'))
 //app.use('/static', express.static('public'))
 const server = http.createServer(app)
@@ -26,21 +27,9 @@ gameServer.define('game_room', GameRoom);
 gameServer.define('lobby_room', LobbyGameRoom)
 app.use('/colyseus', monitor())
 
-/*
-app.get('/game', (req, res) => {
-    res.redirect("ws://" + 'localhost' + ":8000")
-})
-*/
-
 app.get('/', (req,res)=>{
     res.sendFile(path.join(__dirname, '../client/index.html'))
 })
-
-app.get('/lobby/:id', (req, res)=>{
-    var lobby_id = req.params.id;
-    // TODO: sending to the lobby at the url 
-})
-
 
 
 gameServer.listen(port)
--- src/server/rooms/LobbyGameRoom.ts
+++ src/server/rooms/LobbyGameRoom.ts
@@ -2,28 +2,47 @@
 import { Schema, type } from "@colyseus/schema";
 import { Client, LobbyRoom } from "colyseus";
 
-// Customization for the joining or creating room
-export function isValidId(id: string) { return id && /^[a-zA-Z0-9_\-]{9}$/.test(id); } 
-
 class LobbyState extends Schema {
-    @type('number') counter : number = 0; // counter number of total players
+    @type('number') connectionsCounter : number = 0; // counter number of total players
 }
 
+
 export class LobbyGameRoom extends LobbyRoom {
+
+    // metadata
+    private usernameList : string[] = []
+    private lobbyCounter : number = 0
+    removeUsername(username : string ){
+        this.usernameList = this.usernameList.filter(function(e) { return e !== username })
+    }
+
     async onCreate(options : any) {
         await super.onCreate(options);
         this.setState(new LobbyState()); // init the distributed state
         this.roomId = options.roomId
+        this.onMessage("usernamesRequest", (client, message) => {
+            this.broadcast("+", this.usernameList) // broadcast on group of players for missplaced update
+        });
     }
 
     onJoin(client: Client, options : any) {
         super.onJoin(client, options);
         client.userData = {username : options.username} // distributed data
-        this.state.counter++
+        this.usernameList.push(options.username)
+        this.lobbyCounter++
+        console.log('Lobby Counter Player: ' + this.lobbyCounter)
+        this.broadcast("players", this.lobbyCounter)
+        this.broadcast("+", this.usernameList) // adding username in the list
     }
 
     onLeave(client : any) {
         super.onLeave(client);
+        this.removeUsername(client.userData.username)
+        this.lobbyCounter--
+        this.broadcast("players", this.lobbyCounter)
+        this.broadcast("-", this.usernameList) // remove client username in the distributed list
         this.state.counter--;
     }
+    
+    
 }
