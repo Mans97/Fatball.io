258b8c335cc6c954f60f57a4c661f16cbcf9cb1d
ema.fazz <ema.fazz@gmail.com>
2022-01-07 12:19:52 +0100

    aggiunto l'alpha al colore del giocatore, modificato il font del nome

--- src/client/static/ts/HelloWorldScene.ts
+++ src/client/static/ts/HelloWorldScene.ts
@@ -46,9 +46,9 @@ export default class HelloWorldScene extends Phaser.Scene {
 
       //create the player with text inside
       var circle_player = this.add
-        .circle(0, 0, player.radius, player.color)
+        .circle(0, 0, player.radius, player.color, 0.4)
         .setStrokeStyle(3, player.border_color);
-      var playerNick = this.add.text(0, 0, 'Pippo', { fontSize: '32px', color: '#000' });
+      var playerNick = this.add.text(0, 0, 'Pippo', { fontFamily: 'Helvetica', fontSize: '32px', color: '#000' });
       playerNick.x = playerNick.x - playerNick.width/2
       playerNick.y = playerNick.y - playerNick.height/2
 
